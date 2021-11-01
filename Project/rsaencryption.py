f8774eb71ee46f17750aeba9818c21a6081c95db
Nitya Sunkad <52294194+nsunkad@users.noreply.github.com>
2021-10-31 21:23:48 -0500

    Create rsaencryption.py

---
+++ Project/rsaencryption.py
@@ -0,0 +1,40 @@
+# pip install pycryptodome
+
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto import Random
+
+"""helpful reading: https://www.pythonpool.com/rsa-encryption-python/, 
+https://pycryptodome.readthedocs.io/en/latest/src/public_key/rsa.html
+implementation: https://stackoverflow.com/questions/30056762/rsa-encryption-and-decryption-in-python"""
+
+
+random_generator = Random.new().read
+key = RSA.generate(2048, random_generator) #generate public and private keys
+
+public_key = key.publickey # pub key export for exchange
+
+encrypted = public_key.encrypt('encrypt this message', 32)
+#message to encrypt is in the above line 'encrypt this message'
+
+print('encrypted message: ' + encrypted) #ciphertext
+
+f = open ('encryption.txt', 'w')
+f.write(str(encrypted)) #write ciphertext to file
+f.close()
+
+#decrypted code below
+
+f = open ('encryption.txt', 'r')
+message = f.read()
+
+decrypted = key.decrypt(message)
+
+print('decrypted: ' + decrypted)
+
+f = open ('encryption.txt', 'w')
+f.write(str(message))
+f.write(str(decrypted))
+f.close()
+
+
